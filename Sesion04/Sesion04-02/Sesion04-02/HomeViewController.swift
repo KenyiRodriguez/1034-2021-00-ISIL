17ead9e30e416c2de982fa149d8ca096772cd010
Kenyi Rodriguez <krodriguez@solera.pe>
2021-01-18 21:44:59 -0500

    Sesion04-02

---
+++ Sesion04/Sesion04-02/Sesion04-02/HomeViewController.swift
@@ -0,0 +1,68 @@
+//
+//  HomeViewController.swift
+//  Sesion04-02
+//
+//  Created by Kenyi Rodriguez on 18/01/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBAction func tapToCloseKeyboard(_ sender: Any) {
+        self.view.endEditing(true)
+    }
+ 
+    
+    //MARK: - Keyboard Methods
+    @objc func keyboardWillShow(_ notification: Notification) {
+    
+        let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect ?? .zero
+        let keyboardSize = keyboardFrame.size
+        
+        print(keyboardSize)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        
+        print("EL TECLADO SE VAAAAA!!!!")
+    }
+    
+    
+    //MARK: - Life Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillShow(_:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardWillHide(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
+/*
+
+ viewDidLoad        ->  terminar de cargar
+ viewWillAppear     ->  empieza a aparecer
+ viewDidAppear      ->  termina de aparecer
+ viewWillDisappear  ->  empieza a desaparecer
+ viewDidDisappear   ->  termina de desaparecer
+ deInit             ->  cuando se destruye
+ 
+ */
